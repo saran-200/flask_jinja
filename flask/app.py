a2fafc05e751431b57bb00f25aa35fbc020c4320
saran-200 <saran.r.it.mec@gmail.com>
2024-06-05 15:09:12 -0400

    apppp

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ def hello():
     return "<h1>Annamalai</h1>"
 @app.route("/karthi")
 def hai():
-    return "Karthi"
+    return "Karthikeyan"
 
 if __name__ =="__main__":
     app.run(debug=True,port=2004)
